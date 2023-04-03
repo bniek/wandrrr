185009606901d8dc838acd99f0f3fec65c3abb6b
charlene <charlene.xu92@gmail.com>
2023-04-03 16:56:36 -0700

    created and tested get_all/update/delete endpoints

--- wandrrr/routers/journal_entries.py
+++ wandrrr/routers/journal_entries.py
@@ -1,43 +1,45 @@
 #from queries.journal_entries import
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response, HTTPException
+from typing import List, Optional, Union
 from auth.auth_bearer import JWTBearer
+from queries.journal_entries import (
+    Error,
+    PostIn,
+    PostOut,
+    WandrrrRepository,
+)
 
 router = APIRouter()
 
 
-post = [
-    {
-        "id": 1,
-        "title": "title1",
-        "location": "location1"
-    },
-    {
-        "id": 2,
-        "title": "title2",
-        "location": "location2"
-    }
-]
 
-@router.get("/wandrrrs/", tags=["post"])
-def get_list():
-      return{
-            "data": post
-      }
+@router.get("/wandrrrs/", response_model=Union[List[PostOut], Error])
+def get_all_posts(
+     repo: WandrrrRepository = Depends(),
+):
+    return repo.get_all()
 
-@router.get("/wandrrrs/{id}", tags=["post"])
-def get_post(id : int):
 
-        return {
-            "data": post
-        }
-@router.put("/wandrrrs/{id}")
-def edit_post(id : int):
-    return()
+@router.put("/wandrrrs/{wandrrrs_id}", response_model=Union[PostOut, Error])
+def edit_post(
+    wandrrrs_id : int,
+    post: PostIn,
+    repo: WandrrrRepository = Depends(),
+) -> Union[Error, PostOut]:
+    return repo.update(wandrrrs_id, post)
+    # existing_wandrrr = repo.get_one(wandrrrs_id)
+    # if existing_wandrrr is None:
+    #     raise HTTPException(status_code=404, detail="wandrrr not found")
+    # else:
+    #     return repo.update(wandrrrs_id, wandrrr)
 
-@router.delete("/wandrrrs/{id}")
-def delete_post(id : int):
-    return()
+@router.delete("/wandrrrs/{wandrrrs_id}", response_model=bool)
+def delete_post(
+    wandrrrs_id : int,
+    repo: WandrrrRepository = Depends(),
+) -> bool:
+    return repo.delete(wandrrrs_id)
 
-@router.post("/wandrrrs/", dependencies=[Depends(JWTBearer())])
-def create_post():
-    return()
+# @router.post("/wandrrrs/", dependencies=[Depends(JWTBearer())])
+# def create_post():
+#     return()
