057a3b3543214f8e36e33227f7c96cdc184c127e
charlene <charlene.xu92@gmail.com>
2023-04-19 16:34:11 -0700

    landing page done

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -11,7 +11,7 @@ import AccessError from './access-error.js';
 import NewWandrrrForm from './NewWandrrrForm';
 import UpdateWandrrr from './UpdateWandrrr';
 import WandrrrsList from './WandrrrsList.js';
-
+import LandingPage from './LandingPage.js';
 
 
 
@@ -23,7 +23,7 @@ function App(props) {
         <Nav user={user} />
         <div className="container">
           <Routes>
-            <Route path="/" element={<Auth />} />
+            <Route path="/" element={<LandingPage />} />
             <Route path="/signup" element={<Auth />} />
             <Route path="/login" element={<Login />} />
             <Route path="new" element={<NewWandrrrForm user={user} />} />
