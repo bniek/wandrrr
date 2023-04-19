91e1c89c0afa898abfaf056afbd79ca36ff2f7be
Sinlin <sinlinyeo@gmail.com>
2023-04-19 09:46:27 +0800

    Some fixes based on create form

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,85 +1,34 @@
-
-// import { useEffect, useState } from "react";
-// import Construct from "./Construct.js";
-// import ErrorNotification from "./ErrorNotification";
-// import "./App.css";
-
-// function App() {
-//   const [launchInfo, setLaunchInfo] = useState([]);
-//   const [error, setError] = useState(null);
-
-//   useEffect(() => {
-//     async function getData() {
-//       let url = `${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/api/launch-details`;
-//       console.log("fastapi url: ", url);
-//       let response = await fetch(url);
-//       console.log("------- hello? -------");
-//       let data = await response.json();
-
-//       if (response.ok) {
-//         console.log("got launch data!");
-//         setLaunchInfo(data.launch_details);
-//       } else {
-//         console.log("drat! something happened");
-//         setError(data.message);
-//       }
-//     }
-//     getData();
-//   }, []);
-
-//   return (
-//     <div>
-//       <ErrorNotification error={error} />
-//       <Construct info={launchInfo} />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
-
 import { useContext } from 'react';
-import { Routes, Route, NavLink } from "react-router-dom";
-import useToken, { AuthContext, AuthProvider } from '@galvanize-inc/jwtdown-for-react';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import useToken, { AuthContext } from '@galvanize-inc/jwtdown-for-react';
 import Auth from "./Auth.js";
 import "./App.css";
 import useUser from "./useUser";
+import NewWandrrrForm from './NewWandrrrForm';
+import UpdateWandrrr from './UpdateWandrrr';
+import Nav from './Nav';
 
-function App() {
+
+
+function App(props) {
   const { token } = useContext (AuthContext);
   const user = useUser(token);
-  const { logout } = useToken();
+  // console.log(user)
     return (
       <div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-around",
-            width: "80%",
-            margin: "auto",
-          }}
-        >
-          <NavLink to="/create-wandrrr">new wandrrr</NavLink>
-          <NavLink to="my-wandrrrs/">my  wandrrrs</NavLink>
-          {token ? (
-            <button onClick={logout}>Log out</button>
-          ) : (
-            <>
-              <NavLink to="/login">login</NavLink>
-              <NavLink to="/signup">sign up</NavLink>
-            </>
-          )}
+        <Nav />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Auth />} />
+            <Route path="/signup" element={<Auth />} />
+            <Route path="/login" element={<Auth />} />
+            <Route path="new" element={<NewWandrrrForm user={user} />} />
+            <Route path="edit/" element={<UpdateWandrrr />} />
+            <Route path="new" element={<NewWandrrrForm  />} />
+            <Route path="edit/" element={<UpdateWandrrr />} />
+          </Routes>
         </div>
-        <Routes>
-          <Route path="/" element={<Auth />} />
-          <Route path="/signup" element={<Auth />} />
-          <Route path="/login" element={<Auth />} />
-        </Routes>
       </div>
-
     );
 }
 
