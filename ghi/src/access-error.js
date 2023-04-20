ecd639cc707a5c9197590eda3b9d9a72dca5c155
Elaine <elainah@gmail.com>
2023-04-19 17:06:35 -0700

    fixed error page and ready to merge for landing page

--- ghi/src/access-error.js
+++ ghi/src/access-error.js
@@ -3,7 +3,7 @@ function AccessError (){
         <div align="center">
             I am sorry. You do not have access to view this post.
 <br></br>
-        <button a href= "/wandrrrrs" className="btn btn-active btn-ghost">Return to List of Wandrrrs</button>
+        <button a href= "/wandrrrs" className="btn btn-active btn-ghost">Return to List of Wandrrrs</button>
 
         </div>
 
