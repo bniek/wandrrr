a4b4382e8afc5254d84f16afff430c2cda080369
charlene <charlene.xu92@gmail.com>
2023-04-25 11:22:36 -0700

    alter navigate path to /new instead of /wandrrr/new

--- ghi/src/WandrrrsList.js
+++ ghi/src/WandrrrsList.js
@@ -44,7 +44,7 @@ function WandrrrsList() {
                     </div>
                     <div className="content-center">
 
-                        <button onClick={() => navigate(`/wandrrr/new`)} className="btn btn-primary gap-2">
+                        <button onClick={() => navigate(`/new`)} className="btn btn-primary gap-2">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
                             Create your first post
                         </button>
