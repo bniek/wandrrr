1fc1bf6770ca5e1941c8395d03519fcfa4956c7c
charlene <charlene.xu92@gmail.com>
2023-04-26 17:22:26 -0700

    added styling to the login form

--- ghi/src/LoginForm.js
+++ ghi/src/LoginForm.js
@@ -30,41 +30,58 @@ function Login() {
     }
   };
 
-  // if (token != null) {
-  //   window.location.href = "/wandrrrs";
-  // }
-
   const { username = "", password = "" } = formData;
+
   return (
-    <div align="center">
-      <form>
-        <div>
-          <label>
-            Username:
+    <div className="contain">
+      <div
+      className="mx-auto py-20">
+        <form
+        className="bg-[#AFDAFE] g-gray-100 rounded-lg shadow-lg max-w-xl m-auto py-10 mt-10 px-12 border"
+        onSubmit={handleSubmit}
+        id="create-login-form"
+        >
+          <div className="form-floating mb-3 py-20">
+            <h1
+            className="text-6xl text-[#FFFFFF] text-center"
+            style={{ fontFamily: 'Instrument Serif' }}>
+              Log In!
+            </h1>
+          </div>
+          <div className="form-floating mb-3">
+            <label className="py-2 text-[#FFFFFF]" htmlFor="username">Username*</label>
             <input
-              type="text"
-              name="username"
-              value={username}
-              onChange={handleFormChange}
+            onChange={handleFormChange}
+            placeholder="Username"
+            required
+            type="text"
+            name="username"
+            id="username"
+            value={username}
+            className="bg-[#FFFFFF] border-solid border-[#FFFFFF] border py-2 px-4 w-full rounded text-gray-700"
             />
-          </label>
-        </div>
-        <div>
-          <label>
-            Password:
+          </div>
+          <div className="form-floating mb-3">
+            <label className="py-2 text-[#FFFFFF]" htmlFor="password">Password* </label>
             <input
-              type="password"
-              name="password"
-              value={password}
-              onChange={handleFormChange}
+            onChange={handleFormChange}
+            placeholder="Password"
+            required
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            className="bg-[#FFFFFF] border-solid border-[#FFFFFF] border py-2 px-4 w-full rounded text-gray-700"
             />
-          </label>
-        </div>
-      </form>
-      <br></br>
-      <button className="btn btn-active btn-ghost" onClick={handleSubmit}>
-        {isLogIn ? "Log in" : "Sign up"}
-      </button>
+          </div>
+           <button
+           className="mt-4 w-full bg-[#AFDAFE] hover:text-[#AFDAFE] hover:bg-[#FFFFFF] text-[#FFFFFF] rounded-lg border-2 border-[#FFFFFF] py-3 px-6 font-semibold text-md rounded"
+           type="submit" style={{ fontFamily: 'Instrument Serif' }}
+           >
+            {isLogIn ? "Log in" : "Sign up"}
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
